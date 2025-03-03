572ce02e001dae808fd2696818f3c2494318fea5
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-03 17:55:06 +0530

    Time: 66 ms (0.00%) | Memory: 50 MB (100.00%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.c
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.c
@@ -1,46 +1,27 @@
 #include <stdlib.h>
 
-void swaprows(int **score, int r1, int r2, int n)
-{
-    int i, temp;
-    for (i = 0; i < n; i++)
-    {
-        temp = score[r1][i];
+void swaprows(int **score, int r1, int r2, int n) {
+    for (int i = 0; i < n; i++) {
+        int temp = score[r1][i];
         score[r1][i] = score[r2][i];
         score[r2][i] = temp;
     }
 }
 
-int** sortTheStudents(int** score, int m, int* n, int k, int* returnSize, int** returnColumnSizes)
-{
-    // Allocate memory for the new sorted matrix
-    int** sortedMatrix = (int**)malloc(m * sizeof(int*));
-    for (int i = 0; i < m; i++)
-    {
-        sortedMatrix[i] = (int*)malloc((*n) * sizeof(int));
-        for (int j = 0; j < *n; j++)
-        {
-            sortedMatrix[i][j] = score[i][j]; // Copy original matrix
-        }
-    }
-    // Sort using selection sort (descending order)
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = i + 1; j < m; j++)
-        {
-            if (sortedMatrix[i][k] < sortedMatrix[j][k])
-            {
-                swaprows(sortedMatrix, i, j, *n);
+int** sortTheStudents(int** score, int m, int* n, int k, int* returnSize, int** returnColumnSizes) {
+    // Sort the matrix in-place based on the k-th column in descending order
+    for (int i = 0; i < m - 1; i++) {
+        for (int j = i + 1; j < m; j++) {
+            if (score[i][k] < score[j][k]) {
+                swaprows(score, i, j, *n);
             }
         }
     }
-    // Allocate memory for returnColumnSizes and set values
+    // Set return values
     *returnSize = m;
-    *returnColumnSizes = (int*)malloc(m * sizeof(int));
-    for (int i = 0; i < m; i++)
-    {
-        (*returnColumnSizes)[i] = *n;
+    *returnColumnSizes = (int*)malloc(m * sizeof(int)); // Allocate memory
+    for (int i = 0; i < m; i++) {
+        (*returnColumnSizes)[i] = *n; // Each row has 'n' columns
     }
-
-    return sortedMatrix;
+    return score;
 }
