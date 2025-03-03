926ca97ad1a83b9b56dfe06ba58813044ae3bf9a
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-03 17:58:10 +0530

    Time: 0 ms (100.00%) | Memory: 50 MB (60.00%) - LeetSync

--- 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.c
+++ 2631-sort-the-students-by-their-kth-score/sort-the-students-by-their-kth-score.c
@@ -1,46 +1,29 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-void swaprows(int **score, int r1, int r2, int n)
-{
-    int i, temp;
-    for (i = 0; i < n; i++)
-    {
-        temp = score[r1][i];
-        score[r1][i] = score[r2][i];
-        score[r2][i] = temp;
-    }
+// Global variable for the column index
+int sortColumn;
+
+// Custom comparator function for qsort
+int compare(const void* a, const void* b) {
+    int* row1 = *(int**)a;
+    int* row2 = *(int**)b;
+
+    return row2[sortColumn] - row1[sortColumn]; // Descending order
 }
 
-int** sortTheStudents(int** score, int m, int* n, int k, int* returnSize, int** returnColumnSizes)
-{
-    // Allocate memory for the new sorted matrix
-    int** sortedMatrix = (int**)malloc(m * sizeof(int*));
-    for (int i = 0; i < m; i++)
-    {
-        sortedMatrix[i] = (int*)malloc((*n) * sizeof(int));
-        for (int j = 0; j < *n; j++)
-        {
-            sortedMatrix[i][j] = score[i][j]; // Copy original matrix
-        }
-    }
-    // Sort using selection sort (descending order)
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = i + 1; j < m; j++)
-        {
-            if (sortedMatrix[i][k] < sortedMatrix[j][k])
-            {
-                swaprows(sortedMatrix, i, j, *n);
-            }
-        }
-    }
-    // Allocate memory for returnColumnSizes and set values
+int** sortTheStudents(int** score, int m, int* n, int k, int* returnSize, int** returnColumnSizes) {
+    // Set the global sorting column
+    sortColumn = k;
+
+    // Sort the rows using qsort
+    qsort(score, m, sizeof(int*), compare);
+
+    // Allocate memory for returnColumnSizes
     *returnSize = m;
     *returnColumnSizes = (int*)malloc(m * sizeof(int));
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++) {
         (*returnColumnSizes)[i] = *n;
     }
-
-    return sortedMatrix;
+    return score;
 }
