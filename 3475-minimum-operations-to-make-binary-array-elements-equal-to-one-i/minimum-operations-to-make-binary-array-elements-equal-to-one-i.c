42a01511773cbb7b4ae8f4fc4c5643194cd8431d
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-19 21:13:14 +0530

    Time: 3 ms (94.74%) | Memory: 17.5 MB (31.58%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.c
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.c
@@ -1,24 +1,31 @@
 int minOperations(int* arr, int n)
 {
     int count=0;
-    for(int i=0;i<n-2;i++){
-        if(arr[i]==0){
-            if(arr[i+1]==0){
+    for(int i=0;i<n-2;i++)
+    {
+        if(arr[i]==0)
+        {
+            if(arr[i+1]==0)
+            {
                 arr[i+1]=1;
             }
-            else{
+            else
+            {
                 arr[i+1]=0;
             }
-            if(arr[i+2]==0){
+            if(arr[i+2]==0)
+            {
                 arr[i+2]=1;
             }
-            else{
+            else
+            {
                 arr[i+2]=0;
             }
             count++;
         }
     } 
-    if(arr[n-1]==0||arr[n-2]==0){
+    if(arr[n-1]==0||arr[n-2]==0)
+    {
         return -1;
     }  
     return count;
