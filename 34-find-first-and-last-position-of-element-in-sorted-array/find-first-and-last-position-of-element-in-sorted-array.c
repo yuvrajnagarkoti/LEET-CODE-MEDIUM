4113580708e965b694618881eb5d971389d80e90
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-21 13:19:10 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (98.48%) - LeetSync

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int* searchRange(int* nums, int n, int target, int* returnSize) {
+    int *a = (int *)malloc(2 * sizeof(int));
+    a[0] = -1;
+    a[1] = -1;
+    // Find first occurrence
+    for (int i = 0; i < n; i++)
+    {
+        if (nums[i] == target)
+        {
+            a[0] = i;
+            break;
+        }
+    }
+    // Find last occurrence
+    for (int j = n - 1; j >= 0; j--)
+    {
+        if (nums[j] == target)
+        {
+            a[1] = j;
+            break;
+        }
+    }
+    *returnSize = 2;
+    return a;
+}
