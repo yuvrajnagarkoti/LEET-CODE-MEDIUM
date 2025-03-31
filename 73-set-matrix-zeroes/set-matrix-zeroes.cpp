0282bd6a8f43c0aefce2b83664f01b933e6563f4
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-03-31 14:03:01 +0530

    Time: 2 ms (13.88%) | Memory: 19.1 MB (7.22%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,10 +1,10 @@
 class Solution
 {
 public:
-    void zero(vector<vector<int>>& matrix, vector<vector<int>>& temp, int k1, int k2)
+    void zero(vector<vector<int>>& temp, int k1, int k2)
     {
-        int rows = matrix.size();
-        int cols = matrix[0].size();
+        int rows = temp.size();
+        int cols = temp[0].size();
         for (int i = 0; i < cols; i++)
         {
             temp[k1][i] = 0;
@@ -19,18 +19,17 @@ public:
     {
         int rows = matrix.size();
         int cols = matrix[0].size();
-        vector<vector<int>> temp = matrix; // Copy original matrix to avoid modifying while iterating
+        vector<vector<int>> temp = matrix;
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++)
             {
                 if (matrix[i][j] == 0)
                 {
-                    zero(matrix, temp, i, j);
+                    zero(temp, i, j);
                 }
             }
         }
-        // Copy modified temp back to matrix
         matrix = temp;
     }
 };
