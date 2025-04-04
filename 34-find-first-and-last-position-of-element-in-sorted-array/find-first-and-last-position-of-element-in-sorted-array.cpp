0870e98e267d5a3b7c53b5f7574a6ced59a80c45
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-04-04 20:40:20 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (17.92%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,43 +1,26 @@
+
 class Solution {
+private:
+    int lower_bound(vector<int>& nums, int low, int high, int target){
+        while(low <= high){
+            int mid = (low + high) >> 1;
+            if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int first = findFirst(nums, target);
-        int last = findLast(nums, target);
-        return {first, last};
-    }
-    
-private:
-    // Binary search to find the first occurrence of target
-    int findFirst(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-        int index = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] >= target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-            if (nums[mid] == target)
-                index = mid;
+        int low = 0, high = nums.size()-1;
+        int startingPosition = lower_bound(nums, low, high, target);
+        int endingPosition = lower_bound(nums, low, high, target + 1) - 1;
+        if(startingPosition < nums.size() && nums[startingPosition] == target){
+            return {startingPosition, endingPosition};
         }
-        return index;
-    }
-    
-    // Binary search to find the last occurrence of target
-    int findLast(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
-        int index = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] <= target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-            if (nums[mid] == target)
-                index = mid;
-        }
-        return index;
+        return {-1, -1};
     }
 };
