addfae8b3edb5aa3f5fb030c1b8538dcc0bc66f3
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-04-29 20:51:58 +0530

    Time: 3 ms (20.52%) | Memory: 71.7 MB (81.34%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -9,15 +9,10 @@ public:
         for (int i = 0; i < nums.size(); i++)
         {
             currentSum += nums[i];
-
             if (currentSum > maxSum)
-            {
                 maxSum = currentSum;
-            }
             if (currentSum < 0)
-            {
                 currentSum = 0;
-            }
         }     
         return maxSum;
     }
