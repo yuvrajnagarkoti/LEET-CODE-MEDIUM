bedabd4db2da3dc675e3b6f348ab45e884393689
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-05-02 19:16:05 +0530

    Time: 24 ms (28.57%) | Memory: 22.9 MB (39.64%) - LeetSync

--- 868-push-dominoes/push-dominoes.cpp
+++ 868-push-dominoes/push-dominoes.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
-    string pushDominoes(string dominoes) {
+    string pushDominoes(string dominoes)
+    {
         int n = dominoes.size();
         vector<int> forces(n, 0);
 
         // Left to right pass (apply rightward forces)
         int force = 0;
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; ++i)
+        {
             if (dominoes[i] == 'R') force = n;
             else if (dominoes[i] == 'L') force = 0;
             else force = max(force - 1, 0);
@@ -15,7 +17,8 @@ public:
 
         // Right to left pass (apply leftward forces)
         force = 0;
-        for (int i = n - 1; i >= 0; --i) {
+        for (int i = n - 1; i >= 0; --i)
+        {
             if (dominoes[i] == 'L') force = n;
             else if (dominoes[i] == 'R') force = 0;
             else force = max(force - 1, 0);
@@ -24,12 +27,12 @@ public:
 
         // Build final state
         string result;
-        for (int f : forces) {
+        for (int f : forces)
+        {
             if (f > 0) result += 'R';
             else if (f < 0) result += 'L';
             else result += '.';
         }
-
         return result;
     }
 };
