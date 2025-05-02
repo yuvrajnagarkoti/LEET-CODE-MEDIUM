e2704cb1a7a7c2b6dd27bda3b9e769498aa08213
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-05-02 19:14:46 +0530

    Time: 15 ms (62.86%) | Memory: 23 MB (33.93%) - LeetSync

--- 868-push-dominoes/push-dominoes.cpp
+++ 868-push-dominoes/push-dominoes.cpp
@@ -1,53 +1,35 @@
 class Solution {
 public:
-    string pushDominoes(string dominoes)
-    {
-        int prev=-1,i=0;
-        while(i<dominoes.size())
-        {
-            if(dominoes[i]!='.')
-            {
-                prev=i;
-                i++;
-                continue;
-            }
-            int cur=i;
-            while(i<dominoes.size()&&dominoes[i]=='.') i++;
-            if(prev==-1||dominoes[prev]=='L')
-            {
-                if(i<dominoes.size()&&dominoes[i]=='L')
-                {
-                    while(cur<i)
-                    {
-                        dominoes[cur]='L';
-                        cur++;
-                    }
-                }
-            }
-            else
-            {
-                if(i>=dominoes.size()||dominoes[i]=='R')
-                {
-                    while(cur<i)
-                    {
-                        dominoes[cur]='R';
-                        cur++;
-                    }
-                }
-                else
-                {
-                    while(cur<i)
-                    {
-                        int val1=cur-prev,val2=i-cur;
-                        if(val1<val2) dominoes[cur]=dominoes[prev];
-                        else if(val1>val2) dominoes[cur]=dominoes[i];
-                        cur++;
-                    }
-                }    
-            }
-            prev=i;
-            i++;
+    string pushDominoes(string dominoes) {
+        int n = dominoes.size();
+        vector<int> forces(n, 0);
+
+        // Left to right pass (apply rightward forces)
+        int force = 0;
+        for (int i = 0; i < n; ++i) {
+            if (dominoes[i] == 'R') force = n;
+            else if (dominoes[i] == 'L') force = 0;
+            else force = max(force - 1, 0);
+            forces[i] += force;
         }
-        return dominoes;
+
+        // Right to left pass (apply leftward forces)
+        force = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            if (dominoes[i] == 'L') force = n;
+            else if (dominoes[i] == 'R') force = 0;
+            else force = max(force - 1, 0);
+            forces[i] -= force;
+        }
+
+        // Build final state
+        string result;
+        for (int f : forces) {
+            if (f > 0) result += 'R';
+            else if (f < 0) result += 'L';
+            else result += '.';
+        }
+
+        return result;
     }
 };
