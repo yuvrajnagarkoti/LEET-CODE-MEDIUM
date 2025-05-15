cf654120eb175a7a26bbb88ef0b202479e3a12e6
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-05-16 00:01:57 +0530

    Time: 3 ms (31.57%) | Memory: 14.2 MB (10.93%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums)
+    {
+        map <int,int> count;
+        for(int it : nums)
+        {
+            count[it]++;
+        }
+        for(auto it : count)
+        {
+            if(it.second != 3)
+            {
+                return it.first;
+            }
+        }
+        return 0;
+    }
+};
