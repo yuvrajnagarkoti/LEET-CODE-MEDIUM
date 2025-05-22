f7c3db1622a38249536f8c56bd2191c0b020aa65
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-05-22 21:03:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (92.06%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,7 +1,26 @@
 class Solution {
+    private:
+        double power(double x, long n){
+            if(n == 0) return 1.0;
+
+            if(n==1) return x;
+
+            if(n %2 == 0){
+                return power(x * x , n/2);
+            }
+
+            return x * power(x , n-1);
+        }
 public:
-    double myPow(double x, int n)
-    {
-        return (double) pow(x,n);
+    double myPow(double x, int n) {
+        //your code goes here
+
+        long num = n;
+
+        if(n < 0){
+            return (1.0 / power(x , -1 * num));
+        }
+
+        return power(x , num);
     }
 };
