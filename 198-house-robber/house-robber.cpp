6d15e41d5256e66d3caf166c4230048f52cc4f2e
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-06-24 11:21:47 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (79.48%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,29 +3,15 @@ class Solution
 public:
     int rob(vector<int>& nums)
     {
-        int i,j,n=nums.size();
-        if(n==1)    return nums[0];
-        vector<int> sum(n,0);
-        sum[0] = nums[0];
-        for(i=1;i<n;i++)
-        {
-            int max1=nums[i];
-            for(j=i-2;j>=0;j--)
-            {
-                int temp = nums[i] + sum[j];
-                if(max1 < temp)
-                {
-                    max1 = temp;
-                }
-            }
-            sum[i] = max1;
-        }
-        int max1=INT_MIN;
+        int i,n=nums.size();
+        int left=0,right=0;
+
         for(i=0;i<n;i++)
         {
-            if(sum[i] > max1)
-                max1 = sum[i];
+            int temp = max ( left+nums[i] , right);
+            left = right;
+            right = temp;
         }
-        return max1;
+        return right;
     }
 };
