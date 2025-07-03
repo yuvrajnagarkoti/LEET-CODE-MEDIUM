22c18857e36c2d34de3fc1cafdd7a1823c20d975
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-07-03 21:33:39 +0530

    Time: 4 ms (6.90%) | Memory: 9 MB (81.72%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,39 @@
+#include <climits>
+
+class Solution {
+public:
+    int myAtoi(string s)
+    {
+        int i = 0, flag = 1;
+        long sum = 0;
+        while (i < s.length() && s[i] == ' ')
+            i++;
+
+        // Handle optional '+' or '-'
+        if (i < s.length())
+        {
+            if (s[i] == '-')
+            {
+                flag = -1;
+                i++;
+            }
+            else if (s[i] == '+')
+            {
+                i++;
+            }
+        }
+
+        // Read digits with overflow check
+        while (i < s.length() && s[i] >= '0' && s[i] <= '9')
+        {
+            int digit = s[i] - '0';
+            if (sum > (INT_MAX - digit) / 10)
+            {
+                return (flag == 1) ? INT_MAX : INT_MIN;
+            }
+            sum = sum * 10 + digit;
+            i++;
+        }
+        return (int)(sum * flag);
+    }
+};
