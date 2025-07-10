5b5a14bb3789bcfd846bf871f73de86bae68e77c
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-07-10 15:53:38 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (78.99%) - LeetSync

---
+++ 260-single-number-iii/single-number-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums)
+    {
+        sort(nums.begin(), nums.end());
+    vector<int> ans;
+    for (int i = 0; i < nums.size(); i++) {
+        if (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+            i++;
+        } else {
+            ans.push_back(nums[i]);
+        }
+    }
+
+        return ans;
+    }
+};
