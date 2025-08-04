13f104ce2f253b83bb3c1566817730af95b63193
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-04 17:29:09 +0530

    Time: 42 ms (5.02%) | Memory: 46.4 MB (5.01%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,45 @@
+class Solution
+{
+    public:
+    vector<string> ans;
+
+    bool isvalid(string curr)
+    {
+        int sum=0;
+        for(int i=0;i<curr.length();i++)
+        {
+            if(curr[i] == '(')
+                sum++;
+            else
+                sum--;
+            if(sum < 0)
+                return false;    
+        }
+        if(sum == 0)
+            return true;
+        return false;
+    }
+
+    void solve(string &curr,int n)
+    {
+        if(curr.length() == n*2)
+        {
+            if(isvalid(curr))
+                ans.push_back(curr);
+            return;
+        }
+        curr.push_back('(');
+        solve(curr,n);
+        curr.pop_back();
+        curr.push_back(')');
+        solve(curr,n);
+        curr.pop_back();
+    }
+
+    vector<string> generateParenthesis(int n)
+    {
+        string curr="";
+        solve(curr,n);
+        return ans;
+    }
+};
