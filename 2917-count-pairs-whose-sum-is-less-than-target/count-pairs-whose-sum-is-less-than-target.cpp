a4343a6b7f80ef3828e19eb54dfcda8f7056a7e6
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-11 11:25:14 +0530

    Time: 1 ms (17.11%) | Memory: 24.7 MB (15.01%) - LeetSync

--- 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int target)
     {
-        int count=0;
-        for(int i=0;i<nums.size();i++)
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        int i=0,j=n-1,count=0;
+        while( i!=j )
         {
-            for(int j=i+1;j<nums.size();j++)
+            if(nums[i]+nums[j] < target)
             {
-                if(nums[i]+nums[j] < target)
-                {
-                    count++;
-                }
+                count = count + (j-i);
+                i++;
             }
+            else
+                j--;
         }
         return count;
     }
