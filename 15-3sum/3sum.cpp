1e7c76e50a2a46395187b8d01cf03bad2ee56686
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-25 20:32:29 +0530

    Time: 47 ms (70.04%) | Memory: 29.2 MB (48.26%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,35 +1,37 @@
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums)
-    {
-    vector<vector<int>> ans;
-    sort(nums.begin(), nums.end());
-    int i,low,high;
-    for (i = 0; i < nums.size() - 2; i++)
-    {
-        if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicates for i
-        low = i + 1;
-        high = nums.size() - 1;
-        while (low < high)
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++)
         {
-            int sum = nums[i] + nums[low] + nums[high];
-            if (sum == 0)
-            {
-                ans.push_back({nums[i], nums[low], nums[high]});
-                // skip duplicates for low
-                while (low < high && nums[low] == nums[low + 1]) low++;
-                // skip duplicates for high
-                while (low < high && nums[high] == nums[high - 1]) high--;
-                low++;
-                high--;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0)
+                {
+                    res.push_back({nums[i], nums[left], nums[right]});
+                    // Skip duplicates for left
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    // Skip duplicates for right
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
             }
-            else if (sum < 0)
-                low++;
-            else
-                high--;
         }
-    }
-    return ans;
+        return res;
     }
 };
