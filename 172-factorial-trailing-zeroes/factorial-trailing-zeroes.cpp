809a4fc198fd234be7abc7bc81fc7eb2fd2931c9
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-31 01:45:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (85.89%) - LeetSync

---
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int trailingZeroes(int n)
+    {
+        int sum=0;
+        while(n/5 > 0)
+        {
+            sum =sum+n/5;
+            n = n/5;
+        }
+        return sum;
+    }
+};
