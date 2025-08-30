a6c9f13c14af64491ab32f8ec401ff63a002ce12
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-31 01:13:14 +0530

    Time: 9 ms (6.03%) | Memory: 12.3 MB (63.60%) - LeetSync

---
+++ 274-h-index/h-index.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations)
+    {
+        int ans=0;
+        for(int k=0;k<1001;k++)
+        {
+            int count=0;
+            for(int i=0;i<citations.size();i++)
+            {
+                if(citations[i] >= k)
+                {
+                    count++;
+                }
+            }
+            if(count >= k)
+            {
+                ans=k;
+            }
+            else
+                break;
+        }
+        return ans;
+    }
+};
