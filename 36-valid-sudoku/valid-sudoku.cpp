1f579d9c1cd5d568226d1b45a24e520a91520264
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-31 02:53:55 +0530

    Time: 3 ms (62.69%) | Memory: 22.1 MB (76.98%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,50 +1,46 @@
 class Solution
 {
-    public:
-    static int ctoi(char c)
-    {
-        return c - '0';
-    }
-
+public:
     bool isValidSudoku(vector<vector<char>>& board)
     {
+        // Check rows and columns
         for(int i = 0; i < 9; i++)
         {
             vector<int> rowCount(9, 0);
             vector<int> colCount(9, 0);
-
+            
             for(int j = 0; j < 9; j++)
             {
                 // Check row
                 if(board[i][j] != '.')
                 {
-                    int temp = ctoi(board[i][j]) - 1;
+                    int temp = board[i][j] - '0' - 1;
                     if(rowCount[temp] != 0) return false;
                     rowCount[temp]++;
                 }
                 // Check column
                 if(board[j][i] != '.')
                 {
-                    int temp = ctoi(board[j][i]) - 1;
+                    int temp = board[j][i] - '0' - 1;
                     if(colCount[temp] != 0) return false;
                     colCount[temp]++;
                 }
             }
         }
-        
-        for(int i=0;i<9;i=i+3)
+
+        // Check 3x3 sub-boxes
+        for(int i = 0; i < 9; i += 3)
         {
-            for(int j=0;j<9;j=j+3)
+            for(int j = 0; j < 9; j += 3)
             {
                 vector<int> count(9,0);
-                for(int k=i;k<i+3;k++)
+                for(int k = i; k < i + 3; k++)
                 {
-                    for(int l=j;l<j+3;l++)
+                    for(int l = j; l < j + 3; l++)
                     {
                         if(board[k][l] == '.') continue;
-                        int temp = ctoi(board[k][l])-1;
-                        if(count[temp] != 0)
-                            return false;
+                        int temp = board[k][l] - '0' - 1;
+                        if(count[temp] != 0) return false;
                         count[temp]++;
                     }
                 }
