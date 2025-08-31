eccb315cd11f2c86d7e5e31d6c7242284d249db2
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-08-31 21:13:26 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (94.31%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
-    vector<int> productExceptSelf(vector<int>& nums)
+    vector<int> productExceptSelf(vector<int>& nums) 
     {
-        int i=0,n=nums.size();
-        vector<int> leftsub(n,0);
-        vector<int> rightsub(n,0);
-        vector<int> ans(n,0);
-        leftsub[0]=1;
-        leftsub[1]=nums[0];
-        rightsub[n-1]=1;
-        rightsub[n-2]=nums[n-1];
+        int n = nums.size();
+        vector<int> ans(n, 1);
+        int prefix = 1;
+        for (int i = 0; i < n; i++) 
+        {
+            ans[i] = prefix;
+            prefix *= nums[i];
+        }
 
-        for(i=1;i<n;i++)
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; i--)
         {
-            leftsub[i]=nums[i-1]*leftsub[i-1];
-        }
-        for(i=n-2;i>=0;i--)
-        {
-            rightsub[i]=nums[i+1]*rightsub[i+1];
-        }
-        
-        ans[0]=rightsub[0];
-        ans[n-1]=leftsub[n-1];
-        for(i=1;i<n-1;i++)
-        {
-            ans[i]=leftsub[i]*rightsub[i];
+            ans[i] *= suffix;
+            suffix *= nums[i];
         }
         return ans;
     }
