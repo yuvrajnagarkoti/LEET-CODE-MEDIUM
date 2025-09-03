cad6d16ff1d4b6da27f728309382670ef4b5e052
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2025-09-03 18:58:22 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (75.48%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -3,81 +3,46 @@ public:
     vector<string> letterCombinations(string digit)
     {
         vector<string> ans;
-        vector<vector<char>> mpp;
-        mpp.push_back({});
-        mpp.push_back({});
-        mpp.push_back({'a','b','c'});
-        mpp.push_back({'d','e','f'});
-        mpp.push_back({'g','h','i'});
-        mpp.push_back({'j','k','l'});
-        mpp.push_back({'m','n','o'});
-        mpp.push_back({'p','q','r','s'});
-        mpp.push_back({'t','u','v'});
-        mpp.push_back({'w','x','y','z'});
+        vector<vector<char>> mpp =
+        {
+            {}, {}, {'a','b','c'}, {'d','e','f'}, {'g','h','i'},
+            {'j','k','l'}, {'m','n','o'}, {'p','q','r','s'},
+            {'t','u','v'}, {'w','x','y','z'}
+        };
 
-        int n=digit.size();
-        if(n==1)
-        {
+        int n = digit.size();
+        if(n==0) return ans;
+
+        if(n==1) {
             int d = digit[0]-'0';
-            for(int i=0;i<mpp[d].size();i++ )
-                ans.push_back({mpp[d][i]});
+            for(char c : mpp[d]) ans.push_back({c});
         }
-        else if(n==2)
-        {
-            int d1= digit[0]-'0';
-            int d2= digit[1]-'0';
-            for(int i=0;i<mpp[d1].size();i++)
-            {
-                string temp = "";
-                temp = temp +mpp[d1][i];
-                for(int j=0;j<mpp[d2].size();j++)
-                {
-                    ans.push_back(temp+mpp[d2][j]);
-                }
-            }
+        else if(n==2) {
+            int d1 = digit[0]-'0';
+            int d2 = digit[1]-'0';
+            for(char c1 : mpp[d1])
+                for(char c2 : mpp[d2])
+                    ans.push_back(string()+c1+c2);
         }
-        else if(n==3)
-        {
-            int d1= digit[0]-'0';
-            int d2= digit[1]-'0';
-            int d3= digit[2]-'0';
-            for(int i=0;i<mpp[d1].size();i++)
-            {
-                string temp = "";
-                temp+=mpp[d1][i];
-                for(int j=0;j<mpp[d2].size();j++)
-                {
-                    string t1=temp+mpp[d2][j];
-                    for(int k=0;k<mpp[d3].size();k++)
-                    {
-                        ans.push_back(t1+mpp[d3][k]);
-                    }
-                }
-            }
+        else if(n==3) {
+            int d1 = digit[0]-'0';
+            int d2 = digit[1]-'0';
+            int d3 = digit[2]-'0';
+            for(char c1 : mpp[d1])
+                for(char c2 : mpp[d2])
+                    for(char c3 : mpp[d3])
+                        ans.push_back(string()+c1+c2+c3);
         }
-        else if(n==4)
-        {
-            int d1= digit[0]-'0';
-            int d2= digit[1]-'0';
-            int d3= digit[2]-'0';
-            int d4= digit[3]-'0';
-            for(int i=0;i<mpp[d1].size();i++)
-            {
-                string temp = "";
-                temp+=mpp[d1][i];
-                for(int j=0;j<mpp[d2].size();j++)
-                {
-                    string t1=temp+mpp[d2][j];
-                    for(int k=0;k<mpp[d3].size();k++)
-                    {
-                        string t2=t1+mpp[d3][k];
-                        for(int l=0;l<mpp[d4].size();l++)
-                        {
-                            ans.push_back(t2+mpp[d4][l]);
-                        }
-                    }
-                }
-            }
+        else if(n==4) {
+            int d1 = digit[0]-'0';
+            int d2 = digit[1]-'0';
+            int d3 = digit[2]-'0';
+            int d4 = digit[3]-'0';
+            for(char c1 : mpp[d1])
+                for(char c2 : mpp[d2])
+                    for(char c3 : mpp[d3])
+                        for(char c4 : mpp[d4])
+                            ans.push_back(string()+c1+c2+c3+c4);
         }
         return ans;
     }
