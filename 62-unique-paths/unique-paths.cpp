fc4604f1d98e0197f9c52e8f4594622a6103f474
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-01-24 12:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (72.28%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,25 +1,21 @@
-class Solution
-{
-    public:
-    int dp[101][101] = {-1};
-    int rec(int x,int y,int m,int n)
-    {
-        if(x==m-1 && y==n-1)
-            return 1;
-        if(x == m)
-            return 0;
-        if(y == n)
-            return 0;
-        if(dp[x][y] != -1)
-            return dp[x][y];
-        
-        int left = rec(x+1,y,m,n);
-        int right = rec(x,y+1,m,n);
-        return dp[x][y] = left+right;
-    }
+class Solution {
+public:
     int uniquePaths(int m, int n)
     {
-        memset(dp,-1,sizeof(dp));
-        return rec(0,0,m,n); //x=0,y=0,m,n; 
+        int bup[101][101]={-1};
+        for(int i=0;i<101;i++)
+        {
+            bup[i][0]=1;
+            bup[0][i]=1;
+        }
+        bup[0][0] = 1;
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                bup[i][j] = bup[i][j-1] + bup[i-1][j];
+            }
+        }
+        return bup[m-1][n-1];
     }
 };
