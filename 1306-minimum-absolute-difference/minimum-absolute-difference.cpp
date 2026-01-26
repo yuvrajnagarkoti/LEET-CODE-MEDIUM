b222ad9210d88f34ca63fd8710706a71ddf6def6
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-01-26 09:50:03 +0530

    Time: 12 ms (89.78%) | Memory: 36.8 MB (32.02%) - LeetSync

--- 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
+++ 1306-minimum-absolute-difference/minimum-absolute-difference.cpp
@@ -14,9 +14,9 @@ public:
         vector<vector<int>> ans;
         for(int i=0;i<arr.size()-1;i++)
         {
-            if(abs(arr[i]-arr[i+1]) == min)
+            if(arr[i+1]-arr[i] == min)
             {
-                ans.push_back({arr[i],arr[i+1]});
+                ans.push_back( {arr[i],arr[i+1]} );
             }
         }
         return ans;
