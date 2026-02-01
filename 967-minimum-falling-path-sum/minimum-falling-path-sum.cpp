ec803571d4c7c2ad3231c27e3a62e2e387ba8448
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-02-01 17:00:25 +0530

    Time: 7 ms (9.69%) | Memory: 15.1 MB (40.94%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.cpp
@@ -1,39 +1,37 @@
 class Solution
 {
 public:
-    int rec(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp)
-    {
-        int n = matrix.size();
-
-        // out of bounds
-        if (j < 0 || j >= n)
-            return 1000000000; // INF
-
-        // last row
-        if (i == n - 1)
-            return matrix[i][j];
-
-        // memoized
-        if (dp[i][j] != INT_MAX)
-            return dp[i][j];
-
-        int left  = rec(i + 1, j - 1, matrix, dp);
-        int down  = rec(i + 1, j,     matrix, dp);
-        int right = rec(i + 1, j + 1, matrix, dp);
-
-        return dp[i][j] = matrix[i][j] + min({left, down, right});
-    }
-
     int minFallingPathSum(vector<vector<int>>& matrix)
     {
         int n = matrix.size();
-        vector<vector<int>> dp(n, vector<int>(n, INT_MAX));
+        vector<vector<int>> dp(n, vector<int>(n, 0));
 
+        // base case: first row
+        for (int j = 0; j < n; j++)
+            dp[0][j] = matrix[0][j];
+
+        // build dp table
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                int upLeft = INT_MAX;
+                int upRight = INT_MAX;
+                int up = dp[i-1][j];
+                if(j>0)
+                    upLeft = dp[i-1][j-1];
+                if(j<n-1)
+                    upRight = dp[i-1][j+1];
+
+                dp[i][j] = matrix[i][j] + min({up, upLeft, upRight});
+            }
+        }
+
+        // answer = min in last row
         int ans = INT_MAX;
         for (int j = 0; j < n; j++)
-        {
-            ans = min(ans, rec(0, j, matrix, dp));
-        }
+            ans = min(ans, dp[n-1][j]);
+
         return ans;
     }
 };
