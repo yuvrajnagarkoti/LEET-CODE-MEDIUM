14ea80c95b113227e0e3a84b7906f46192f983c5
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-02-09 08:59:34 +0530

    Time: 7 ms (79.19%) | Memory: 65.2 MB (81.08%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -9,35 +9,42 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
 class Solution
 {
 public:
-    void inorder(TreeNode* root, vector<TreeNode*>& dfs)
+
+    void inorder(TreeNode* root,vector<TreeNode*> &dfs)
     {
-        if (!root) return;
-        inorder(root->left, dfs);
+        if(root == NULL)
+            return;
+        
+        inorder(root->left,dfs);
         dfs.push_back(root);
-        inorder(root->right, dfs);
+        inorder(root->right,dfs);
     }
 
-    TreeNode* buildtree(vector<TreeNode*>& dfs, int low, int high)
+    void buildtree(TreeNode *&ans,vector<TreeNode*> &dfs,int low,int high)
     {
-        if (low > high) return NULL;
-
-        int mid = low + (high - low) / 2;
-        TreeNode* root = dfs[mid];
-
-        root->left = buildtree(dfs, low, mid - 1);
-        root->right = buildtree(dfs, mid + 1, high);
-
-        return root;
+        if(low > high)
+        {
+            ans=NULL;
+            return;
+        }
+        
+        int mid = low + (high-low)/2;
+        ans = new TreeNode(dfs[mid]->val);
+        buildtree(ans->left,dfs,low,mid-1);
+        buildtree(ans->right,dfs,mid+1,high);
     }
 
     TreeNode* balanceBST(TreeNode* root)
     {
         vector<TreeNode*> dfs;
-        inorder(root, dfs);
-        return buildtree(dfs, 0, dfs.size() - 1);
+        inorder(root,dfs);
+
+        TreeNode *ans = NULL;
+        buildtree(ans,dfs,0,dfs.size()-1);
+
+        return ans;
     }
 };
