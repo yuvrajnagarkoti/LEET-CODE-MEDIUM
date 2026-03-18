0f648ad690afc04d0d66f5b17781b6e700dc34e6
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-03-18 20:26:05 +0530

    Time: 0 ms (100.00%) | Memory: 59.3 MB (58.00%) - LeetSync

--- 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
+++ 784-insert-into-a-binary-search-tree/insert-into-a-binary-search-tree.cpp
@@ -9,33 +9,37 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution
-{
+class Solution {
 public:
-
-    void dfs(TreeNode *&prev,TreeNode *root,int val)
-    {
-        if(root == NULL)    
-            return;
-        
-        prev = root;
-        if(root-> val > val)
-            dfs(prev,root->left,val);
-        else
-            dfs(prev,root->right,val);
-    }
-
     TreeNode* insertIntoBST(TreeNode* root, int val)
     {
-        TreeNode *prev = root;
-        dfs(prev,root,val);
-        if(root == NULL)
-            return new TreeNode(val);
-        
-        if(prev->val > val)
-            prev->left = new TreeNode(val);
-        else
-            prev->right = new TreeNode(val);
+        if(root == NULL) return new TreeNode(val);
+        TreeNode *prev=root;
+        TreeNode *curr=root;
+        while(curr != NULL)
+        {
+            if(curr->val < val)
+            {
+                prev = curr;
+                curr = curr->right;
+            }
+            else
+            {
+                prev = curr;
+                curr = curr->left;
+            }
+        }
+        if(prev != NULL)
+        {
+            if(prev->val > val)
+            {
+                prev->left = new TreeNode(val);
+            }
+            else
+            {
+                prev->right = new TreeNode(val);
+            }
+        }
         return root;
     }
 };
