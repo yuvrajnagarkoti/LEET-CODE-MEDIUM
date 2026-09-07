f94dc48d3c9d6ac07f78eaa071cf9303b944cb74
yuvraj <136042471+yuvrajnagarkoti@users.noreply.github.com>
2026-09-07 18:43:36 +0530

    Time: 26 ms (62.61%) | Memory: 16.3 MB (80.31%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,37 +1,36 @@
 class Solution
 {
     public:
-    void findland(vector<vector<char>> &grid,int i,int j,vector<vector<int>> &vis)
+    void dfs(int i,int j,vector<vector<char>> &grid)
     {
         int n=grid.size(),m=grid[0].size();
-        if(i>=n || j>=m || i<0 || j<0 || vis[i][j]==1 || grid[i][j]=='0') 
+        if(i>=n || j>=m || i<0 || j<0 || grid[i][j]!='1')
             return;
         
-        vis[i][j] = 1;
-        
-        findland(grid,i,j+1,vis);
-        findland(grid,i,j-1,vis);
-        findland(grid,i+1,j,vis);
-        findland(grid,i-1,j,vis);
+        grid[i][j]='2';
+
+        dfs(i+1,j,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
     }
 
     int numIslands(vector<vector<char>>& grid)
     {
+        int count=0;
         int n=grid.size(),m=grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        int ans=0;
+    
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(vis[i][j] == 0 && grid[i][j] == '1')
+                if(grid[i][j] == '1')
                 {
-                    ans++;
-                    findland(grid,i,j,vis);
+                    count++;
+                    dfs(i,j,grid);
                 }
             }
         }
-
-        return ans;
+        return count;
     }
 };
